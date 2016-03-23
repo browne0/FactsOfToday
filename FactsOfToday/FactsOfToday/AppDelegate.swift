c8aaca6ee25aae5281e41c7805975cc5a01728ea
Evan <tragessere@gmail.com>
2016-03-22 20:51:56 -0400

    Notification is Now Fired Daily on the User's Selected Time.

--- FactsOfToday/FactsOfToday/AppDelegate.swift
+++ FactsOfToday/FactsOfToday/AppDelegate.swift
@@ -22,18 +22,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+		let defaults = NSUserDefaults.standardUserDefaults()
+		
+		//User has turned on notifications
+		if defaults.objectForKey(SettingsViewController.notificationKey) != nil &&
+			defaults.boolForKey(SettingsViewController.notificationKey) {
+			
+			let hour = defaults.integerForKey(SettingsViewController.notificationHour)
+			let minute = defaults.integerForKey(SettingsViewController.notificationMinute)
+				
+				
+			let currentTime = NSDate()
+			let gregorian = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+			let components = gregorian!.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: currentTime)
+			components.setValue(hour, forComponent: NSCalendarUnit.Hour)
+			components.setValue(minute, forComponent: NSCalendarUnit.Minute)
+			let timeToFire = gregorian!.dateFromComponents(components)
+			
+			let formatter = NSDateFormatter()
+			formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+			formatter.timeStyle = NSDateFormatterStyle.MediumStyle
+				
+			let notification = UILocalNotification()
+			notification.alertBody = "Check out what important events happened today!"
+			notification.fireDate = timeToFire
+			notification.timeZone = NSTimeZone.defaultTimeZone()
+			notification.repeatInterval = NSCalendarUnit.Day
+			UIApplication.sharedApplication().scheduleLocalNotification(notification)
+		}
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 		
-//		let notification = UILocalNotification()
-//		notification.repeatInterval = NSDay
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+		UIApplication.sharedApplication().cancelAllLocalNotifications()
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
