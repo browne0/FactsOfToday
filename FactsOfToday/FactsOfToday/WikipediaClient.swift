fb92d49afd6b946056fec04dc481b84d7282ef1b
Gary Tragesser <tragessere@gmail.com>
2016-04-04 17:04:51 -0400

    Added method for Wikipedia Image API.

--- FactsOfToday/FactsOfToday/WikipediaClient.swift
+++ FactsOfToday/FactsOfToday/WikipediaClient.swift
@@ -94,4 +94,75 @@ class WikipediaClient {
 		task.resume()
 	}
 	
+	
+	/// Get the thumbnail image URL of a Wikipedia Article given the article title (in its URL format)
+	///
+	/// - note: If the article does not have a thumbnail, the title does not refer to a Wikipedia article, or if any network errors occur, the URL returned in the completion block will be nil
+	///
+	/// - returns: A coordinate object with the location of the subject of the Wikipedia article.
+	class func getThumbnailForArticlesWithTitle(titles: [String], completion: (imageUrls: [NSURL?]) -> () ) {
+		if titles.count <= 0 {
+			completion(imageUrls: [NSURL]())
+			return
+		}
+		
+		var titleConcat = ""
+		
+		for title in titles {
+			titleConcat += title + "|"
+		}
+		titleConcat = String(titleConcat.characters.dropLast())
+		
+		let url = NSURL(string: "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&pilimit=\(titles.count)&pithumbsize=150&format=json&titles=\(titleConcat)")
+		if url == nil {
+			print("error creating URL from string: \(titles)")
+			completion(imageUrls: [NSURL]())
+			return
+		}
+		
+		let request = NSURLRequest(URL: url!)
+		let session = NSURLSession(
+			configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+			delegate: nil,
+			delegateQueue:  NSOperationQueue.mainQueue()
+		)
+		
+		let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+		  completionHandler: { (dataOrNil, response, error) in
+			if error != nil {
+				print("error getting thumbnail for article:\n\(error?.localizedDescription)")
+				completion(imageUrls: [NSURL]())
+				return
+			}
+			
+			do {
+				if let data = dataOrNil {
+					if let responseDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
+						if let pages = responseDictionary["query"]?["pages"] as? NSDictionary {
+							let pageIds = pages.allKeys as! [String]
+							
+							var imageUrls = [NSURL]()
+							
+							for pageId in pageIds {
+								let page = pages[pageId]
+								
+								if page?.objectForKey("thumbnail") != nil {
+									let thumbnail = page!["thumbnail"] as! NSDictionary
+									
+									imageUrls.append(NSURL(string: thumbnail["source"] as! String)!)
+								}
+							}
+							
+							completion(imageUrls: imageUrls)
+						}
+					}
+				}
+			} catch let error as NSError {
+				print("error parsing response, getting coordinates.\n\(error)")
+			}
+			
+			completion(imageUrls: [NSURL]())
+		});
+		task.resume()
+	}
 }
